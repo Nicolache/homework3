d00f1d874a767571c142db09bc63f481618397ad
Nick Snegin <soundriver@mail.ru>
2019-08-19 12:44:59 +0300

    Some logic was added.

--- start.py
+++ start.py
@@ -9,19 +9,42 @@ content_type_convert_dictionaty = {
     'html': 'text/html',
     'jpg': 'image/jpg',
 }
-handlers_map: handlers_map_type = {}
+# handlers_map: handlers_map_type = {}
+handlers_map = {}
+datakinds = {
+    '.html': 'r',
+    '.css': 'r',
+    '.js': 'r',
+    '.gif': 'rb',
+    '.jpg': 'rb',
+    '.jpeg': 'rb',
+    '.mp3': 'rb',
+    '.png': 'rb'
+}
+projectpath = './project'
 response = {}
 
 
+def among_project_extensions(filename):
+    is_in_extensions = False
+    for project_extension in datakinds.keys():
+        if filename.endswith(project_extension):
+            is_in_extensions = True
+    return is_in_extensions
+
+
 def search_project_files() -> Generator[str, None, None]:
-    pass
+    """Gets all files locations inside `projectpath` location.
+
+    Returns a generator.
+    """
+    for dirname, dirs, files in os.walk(projectpath, topdown=True):
+        for file in files:
+            if among_project_extensions(file):
+                yield os.path.join(dirname, file)
 
 
 def alter_page_content():
-    pass
-
-
-def add_handler():
     pass
 
 
@@ -31,13 +54,18 @@ def read_file_content(diskpath: str, datakind: str) -> text_or_bytes_type:
     return file_content
 
 
-def auto_add_handlers():
-    for files in search_project_files():
-        print(files)
-        add_handler()
+def map_handler(filename):
+    print(filename)
+    datakind = datakinds.get(filename.split('.')[1])
+    handlers_map[filename] = dynamic_handler(read_file_content(filename, datakind))
 
 
-def dynamic_handler():
+def auto_map_handlers():
+    for filename in search_project_files():
+        map_handler(filename)
+
+
+def dynamic_handler(data):
     """The function emulates a static handler behaveour
     according to static files found in project's directory.
     """
